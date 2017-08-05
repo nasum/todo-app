ebae06c82c602196e4cf7ea33a9dfe4a27c4c42b
nasum <tomato.wonder.life@gmail.com>
2017-08-05 14:07:06 +0900

    add create user

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,6 @@ module TodoApp
       g.stylesheets false
       g.javascripts false
       g.helper false
-      g.template_engine :haml
       g.test_framework :rspec, view_specs: false, helper_specs: false, fixture: true
       g.fixture_replacement :factory_girl, dir: "spec/factories"
     end
---
+++ spec/factories/users.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  email           :string
+#  password_digest :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+FactoryGirl.define do
+  factory :user do
+    email "MyString"
+    password_digest "MyString"
+  end
+end
